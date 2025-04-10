c457b23a5dd9d4c6d7d8852ac74ebfc34b96c182
Tevyn <tevyn.payne@gmail.com>
2025-04-09 22:46:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,125 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
+import os
+import sys
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+logo_path = resource_path("logo.png")
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(10, 12)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list += [random.choice(letters) for _ in range(nr_letters)]
+    # for char in range(nr_letters):
+    #   password_list.append(random.choice(letters))
+
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    # for char in range(nr_symbols):
+    #   password_list += random.choice(symbols)
+
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+    # for char in range(nr_numbers):
+    #   password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ''.join(password_list)
+    # password = ""
+    # for char in password_list:
+    #   password += char
+
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def get_website():
+    site = website_entry.get()
+    return site
+def get_email():
+    email = email_entry.get()
+    return email
+def get_password():
+    password = password_entry.get()
+    return password
+
+
+def set_details():
+    # Get the user's Documents folder
+    documents_folder = os.path.join(os.path.expanduser("~"), "Documents")
+    data_file_path = os.path.join(documents_folder, "_my_user_data.txt")
+
+    if len(get_website()) == 0 or len(get_email()) == 0 or len(get_password()) == 0:
+        messagebox.showerror(title="Error", message="Please fill all fields")
+    else:
+        is_ok = messagebox.askokcancel(title=get_website(), message=f"These are the details entered: \nEmail:{get_email()}\nPassword:{get_password()}\nIs it ok to save?")
+
+        if is_ok:
+            f = open(data_file_path, 'a')
+            f.write(f"Website: {get_website()}\nEmail/Username: {get_email()}\nPassword: {get_password()}\n\n")
+            f.close()
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock = PhotoImage(file=logo_path)
+canvas.create_image(100, 100, image=lock)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=40)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+
+email_entry = Entry(width=40)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "test@gmail.com")
+
+password_label = Label(text = "Password")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=30, command=set_details)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+
+window.mainloop()
+
